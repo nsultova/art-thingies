50a28b0fd080b07579d741ee7a8fcc92e793f75b
Neda Sultova <neda.sultova@gmail.com>
2023-11-12 21:07:49 +0100

    added .svg save option

--- random_lines/sketch_random_lines.py
+++ random_lines/sketch_random_lines.py
@@ -4,22 +4,24 @@ https://www.generativehut.com/post/generative-art-python-tutorial-for-penplotter
 """
 import vsketch
 import numpy as np
+import datetime
+import os
 
 
 class RandomLinesSketch(vsketch.SketchClass):
     ROWS = 20
     COLS = 25
-    INTERPOLATION_STEPS = 9
+    INTERPOLATION_STEPS = 5
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a3", landscape=False)
         vsk.scale("cm")
-        
+
         # outer loop: list(pts) per row
         # inner loop: create (x,y) for each col in current row
         # [ [(0, 0), (0,1), (0,2)], [(1,0),(1,1), (1,2)], ..]
         all_column_pts = [ 
-            [(row + vsk.random(1.5), col + vsk.random(1)) for col in range(self.COLS)]
+            [(row + vsk.random(5), col + vsk.random(0.3)) for col in range(self.COLS)]
             for row in range(self.ROWS)
         ]
 
@@ -35,6 +37,9 @@ class RandomLinesSketch(vsketch.SketchClass):
                 y_inp = vsk.lerp(y_curr, y_nxt, step/self.INTERPOLATION_STEPS)
 
                 vsk.polygon(zip(x_inp, y_inp))
+        
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        vsk.save(os.path.join("output", f"fig_{timestamp}.svg"))
             
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
