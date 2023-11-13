7ddce7b951ed5625b0495ce0eb4e417c5471d21f
Neda Sultova <neda.sultova@gmail.com>
2023-11-13 19:53:01 +0100

    basic example bezier perlin noise

--- noise_bezier/sketch_noise_bezier.py
+++ noise_bezier/sketch_noise_bezier.py
@@ -1,17 +1,35 @@
 import vsketch
 import numpy as np
-
+import datetime
+import os
 
 class NoiseBezierSketch(vsketch.SketchClass):
-    # Sketch parameters:
-    # radius = vsketch.Param(2.0)
+    N = vsketch.Param(100, 0) 
+    freq = vsketch.Param(0.03, decimals=3) 
+    drift = vsketch.Param(0.06, decimals=2) # offset
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
-        vsk.size("a3", landscape=False)
+        vsk.size("a4", landscape=False)
         vsk.scale("cm")
 
-        # implement your sketch here
-        # vsk.circle(0, 0, self.radius, mode="radius")
+        t = np.arange(self.N) * self.freq
+        perlin = vsk.noise(t, np.arange(8) * 1000)
+
+        for i in range(self.N):
+            offset = i * self.drift
+            vsk.bezier(
+                perlin[i, 0] * 10 + offset,
+                perlin[i, 1] * 10 + offset,
+                perlin[i, 2] * 10 + offset,
+                perlin[i, 3] * 10 + offset,
+                perlin[i, 4] * 10 + offset,
+                perlin[i, 5] * 10 + offset,
+                perlin[i, 6] * 10 + offset,
+                perlin[i, 7] * 10 + offset,
+            )
+        
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        vsk.save(os.path.join("output", f"fig_{timestamp}.svg"))
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
