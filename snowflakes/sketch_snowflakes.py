813e574972db74a99ac23c26fcb284438ce726a8
Neda <neda.sultova@gmail.com>
2023-12-10 12:46:46 +0100

    wip generate_snowflake()

--- snowflakes/sketch_snowflakes.py
+++ snowflakes/sketch_snowflakes.py
@@ -10,21 +10,30 @@ class SnowflakesSketch(vsketch.SketchClass):
     radius = vsketch.Param(value=200, min_value=100, max_value=300, step=50)
     # recursion stop condition
     stop_at_len = vsketch.Param(value=2, min_value=1, max_value=5, step=1)
-    branches = [] #convert to np.array
+    branches = [] #convert 
 
     #drawing parameters elsewhere
-    def generate_branch(self, origin, len, angle):
+    def generate_branch(self, origin, length, angle, stop, var_angle):
         if len < self.stop_at_len:
             return
         
+        if tmp_branch is None:
+            tmp_branch = []
+
         random_angle = np.random.uniform(-np.pi/self.variance_branching_angle, np.pi/self.variance_branching_angle)
         end = origin + np.array([len * np.cos(angle), len * np.sin(angle)])
-        self.branches.append((origin,end))
+        tmp_branch.append((origin,end))
         #plt.plot([origin[0], end[0]], [origin[1], end[1]], color='white', linewidth=thickness)
-        self.generate_branch(origin, len/2, angle - random_angle, self.stop_at_len, self.variance_branching_angle)
-        self.generate_branch(origin, len/2, angle + random_angle, self.stop_at_len, self.variance_branching_angle)
+        self.generate_branch(origin, length/2, angle - random_angle, self.stop_at_len, self.variance_branching_angle)
+        self.generate_branch(origin, length/2, angle + random_angle, self.stop_at_len, self.variance_branching_angle)
 
+        return np.array(tmp_branch)
 
+    def generate_snowflake(self, sym_num, var_angle, radius, stop):
+        np.random.seed(255)
+        for i in range(sym_num):
+            branch = self.generate_branch(np.array([0,0]), self.radius, (2 * np.pi / self.symmetry_number) * i, self.stop_at_len, self.variance_branching_angle)
+            self.branches.extend(branch)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a4", landscape=False)
