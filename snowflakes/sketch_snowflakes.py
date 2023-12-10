8d3ef41531264ba439916d2baf70fa2eb7860dd2
Neda <neda.sultova@gmail.com>
2023-12-10 12:21:15 +0100

    wip implementing generator

--- snowflakes/sketch_snowflakes.py
+++ snowflakes/sketch_snowflakes.py
@@ -4,6 +4,12 @@ import vsketch
 class SnowflakesSketch(vsketch.SketchClass):
     # Sketch parameters:
     # radius = vsketch.Param(2.0)
+    # num arms of snowflake
+    symmetry_number = vsketch.Param(value = 6, min_value=1, max_value=10, step=1) 
+    variance_branching_angle = vsketch.Param(value=1, min_value=1, max_value=10, step=1)
+    radius = vsketch.Param(value=200, min_value=100, max_value=300, step=50)
+    # recursion stop condition
+    stop_at_len = vsketch.Param(value=2, min_value=1, max_value=5, step=1)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a4", landscape=False)
